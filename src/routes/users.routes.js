8e7154f5ef2bb171eda4a21ac7e11693eb80494f
Aditya <aadityavats837@gmail.com>
2024-03-07 00:59:30 +0530

    feat(users.controller.js,users.route.js):added user's watch history pipeline and new routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { loginUser , logoutUser, registerUser,refreshAccessToken } from "../controllers/users.controller.js";
+import { loginUser ,
+    logoutUser,
+    registerUser,
+    refreshAccessToken, 
+    changeCurrentPassord, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getUserWatchHistory } from "../controllers/users.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 
 
@@ -28,6 +39,21 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassord)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getUserWatchHistory)
+
 
 
 export default router
